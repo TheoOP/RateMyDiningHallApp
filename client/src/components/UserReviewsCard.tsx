8d4ad007dfd8661389521e7ec2ecdc07714948fc
EthanPKittell <71111568+EthanPKittell@users.noreply.github.com>
2023-04-08 14:45:09 -0400

    Account page now shows reviews
    not yet specific to the user

--- client/src/components/UserReviewsCard.tsx
+++ client/src/components/UserReviewsCard.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
 import { Rating } from '@mui/material';
 import { auth } from '../config/firebase-config'
 
+import React, { useState, useEffect } from 'react'; //These are all imports to display the user's reviews -Ethan
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from "../config/firebase-config"
+
 const UserReviewsCard = () => {
-  const value = 4;
+  const [reviewLists, setReviewList] = useState([]);
+
+  const reviewsCollectionRef = collection(db, "reviews")
+
+  useEffect(() => {
+    const getReviews = async() => {
+      const data = await getDocs(reviewsCollectionRef);
+      setReviewList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getReviews();
+  })
+
+
+  //const value = 4;
   return (
     <div className='UserReviewsCardContent'>
-      <div className='UserReviewNameTime'>
+      {/* <div className='UserReviewNameTime'>
         <div className='UserReviewNameRating'>
         {auth.currentUser?.displayName}
           <Rating name="read-only" value={value} readOnly
@@ -34,8 +51,28 @@ const UserReviewsCard = () => {
         <button>
         View More Details
       </button>
+      </div> */}
+      <div>{reviewLists.map((review) => {
+        return (
+          <div className="review"> 
+            <hr></hr>
+            <div className="reviewHeader">
+              <div className="title">
+                <span>
+                <h2>{review.title}</h2>
+                <h5>{review.currentTime}</h5>
+                </span>
+              </div>
+            </div>
+            <span>Ratings: Overall-{review.overallRating} Taste-{review.tasteRating} Quality-{review.qualityRating} Selection-{review.selectionRating}</span>
+            <hr></hr>
+            <div className="reviewTextContainer"> {review.reviewText} </div>
+            <h4>By: {review.author.name}</h4>
+            <hr></hr>
+          </div>
+        );
+      })}  
       </div>
-
     </div>
   )
 }
